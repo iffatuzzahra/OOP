0566495f2be98f8794b07f3d2e0f68f7a01ab67c
iffatuzzahra <54696540+iffatuzzahra@users.noreply.github.com>
2020-02-27 10:21:49 +0700

    Update Main.java

--- BangunBangun/src/main/Main.java
+++ BangunBangun/src/main/Main.java
@@ -22,70 +22,67 @@ public class Main {
             System.out.print("Pilih : ");
             pilih = input.nextInt();
 
-            System.out.println("Menu Bangun ");
-            System.out.println("1. Bangun Datar");
-            System.out.println("2. Bangun Ruang");
-            System.out.print("Pilih Bangun : ");
-            pMenu = input.nextInt();
+            if (pilih==1||pilih==2||pilih==3) {
+                System.out.println("Menu Bangun ");
+                System.out.println("1. Bangun Datar");
+                System.out.println("2. Bangun Ruang");
+                System.out.print("Pilih Bangun : ");
+                pMenu = input.nextInt();
+            
+                if (pilih==1&&(pMenu==1||pMenu==2)) {
+                    int sisi;
+                    System.out.print("Masukkan panjang sisi persegi: ");
+                    sisi = input.nextInt();
 
-            if (pilih==1){
-                int sisi;
-                System.out.print("Masukkan panjang sisi persegi: ");
-                sisi = input.nextInt();
+                    if (pMenu==1) {
+                        Persegi persegi = new Persegi(sisi);
+                        persegi.hasil();
+                    }else
+                    if (pMenu==2) {
+                        AlasPersegi dimensi3 = new AlasPersegi(sisi);
+                        System.out.println("Menghitung Balok dan Limas Persegi ");
+                        dimensi3.hasil();
+                    }
+                }else 
+                if (pilih==2&&(pMenu==1||pMenu==2)) {
+                    int alas;
+                    System.out.print("Masukkan panjang sisi segitiga: ");
+                    alas = input.nextInt();
 
-                if (pMenu==1) {
-                    Persegi persegi = new Persegi(sisi);
-                    persegi.hasil();
-                }else
-                if (pMenu==2) {
-                    AlasPersegi dimensi3 = new AlasPersegi(sisi);
-                    System.out.println("Menghitung Balok dan Limas Persegi ");
-                    dimensi3.hasil();
-                }else {
+                    if (pMenu==1) { 
+                        Segitiga segitiga = new Segitiga(alas);
+                        segitiga.hasil();
+                    }else
+                    if (pMenu==2) {
+                        int tinggi3;
+                        System.out.print("Masukkan tinggi bangun ruang beralas segitiga : ");
+                        tinggi3 = input.nextInt();
+                        AlasSegitiga dimensi3 = new AlasSegitiga(alas,tinggi3);
+                        System.out.println("Menghitung Prisma dan Limas Persegi ");
+                        dimensi3.hasil();
+                    }
+                } else 
+                if (pilih==3&&(pMenu==1||pMenu==2)) {
+                    int jari2;
+                    System.out.print("Masukkan panjang jari-jari lingkaran : ");
+                    jari2 = input.nextInt();
+
+                    if (pMenu==1) { 
+                        Lingkaran lingkaran = new Lingkaran(jari2);
+                        lingkaran.hasil();
+                    } else
+                    if (pMenu==2) {
+                        int tinggi;
+                        System.out.print("Masukkan tinggi bangun ruang : ");
+                        tinggi = input.nextInt();
+                        AlasLingkaran dimensi3 = new AlasLingkaran(jari2,tinggi);
+                        System.out.println("Menghitung Tabung dan Kerucut ");
+                        dimensi3.hasil();
+                    }
+                } else {
                     System.out.println("Menu tidak tersedia");
                 }
-            }else 
-            if (pilih==2){
-                int alas;
-                System.out.print("Masukkan panjang sisi segitiga: ");
-                alas = input.nextInt();
-
-                if (pMenu==1) { 
-                    Segitiga segitiga = new Segitiga(alas);
-                    segitiga.hasil();
-                }else
-                if (pMenu==2) {
-                    int tinggi3;
-                    System.out.print("Masukkan tinggi bangun ruang beralas segitiga : ");
-                    tinggi3 = input.nextInt();
-                    AlasSegitiga dimensi3 = new AlasSegitiga(alas,tinggi3);
-                    System.out.println("Menghitung Prisma dan Limas Persegi ");
-                    dimensi3.hasil();
-                }else {
-                    System.out.println("Menu tidak tersedia");
-                }
-
-            } else 
-            if (pilih==3) {
-                int jari2;
-                System.out.print("Masukkan panjang jari-jari lingkaran : ");
-                jari2 = input.nextInt();
-
-                if (pMenu==1) { 
-                    Lingkaran lingkaran = new Lingkaran(jari2);
-                    lingkaran.hasil();
-                }else
-                if (pMenu==2) {
-                    int tinggi;
-                    System.out.print("Masukkan tinggi bangun ruang : ");
-                    tinggi = input.nextInt();
-                    AlasLingkaran dimensi3 = new AlasLingkaran(jari2,tinggi);
-                    System.out.println("Menghitung Tabung dan Kerucut ");
-                    dimensi3.hasil();
-                }else {
-                    System.out.println("Menu tidak tersedia");
-                }
-            }else{
+            } else {
                 System.out.println("Maaf menu tidak tersedia");
             }
             System.out.print("Kembali ke Menu ?? (y/n) : ");
